1ebb855cf38ff77405038701dc24638e1456ea9b
Ona Narbutas <ona.narbutas@gmail.com>
2022-11-21 20:39:46 -0500

    Begin environment set up, on react now

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx']},
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, 'public/'),
+    // port: 3000,
+    // // publicPath: 'http://localhost:3000/dist',
+    // hotOnly: true
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   // template: 'index.html'
+    // }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
