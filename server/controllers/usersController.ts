e7364afee3152911a8be6817be894b6366cf1f25
Ona Narbutas <ona.narbutas@gmail.com>
2023-01-25 18:03:32 -0500

    Server can check sign in request for valid user in db

--- server/controllers/usersController.ts
+++ server/controllers/usersController.ts
@@ -12,15 +12,34 @@ const usersController = {
       const operation: AuthOperation = req.body.operation;
   
       // if the operation is asking to sign up, create a new user account and log user in
-      const username: string = req.body.username;
-      const email: string = req.body.email;
-      const password: string = req.body.password;
+      if (operation === 'signUp') {
+        const username: string = req.body.username;
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+  
+        const hash: string = await bcrypt.hash(password, saltRounds);
+        const queryText = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING user_id, username, email`;
+        const values = [username, email, hash];
+        const response = await db.query(queryText, values);
+        res.locals.newUser = response.rows[0];
 
-      const hash: string = await bcrypt.hash(password, saltRounds);
-      const queryText = `INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING user_id, username, email`;
-      const values = [username, email, hash];
-      const response = await db.query(queryText, values);
-      res.locals.newUser = response.rows[0];
+      // if operation is asking to log in, validate and log in
+      } else {
+        const email: string = req.body.email;
+        const password: string = req.body.password;
+
+        const queryText = `SELECT user_id, username, email, password FROM users WHERE email = $1;`
+        const values = [email];
+        const user = await db.query(queryText, values);
+        res.locals.foundUser = user.rows[0];
+        console.log("found user: ", res.locals.foundUser);
+
+        const verificationResult: boolean = await bcrypt.compare(password, res.locals.foundUser.password);
+        console.log('result: ', verificationResult);
+        return next();
+
+
+      }
       return next();
 
     } catch(error) {
--- server/routes/usersRouter.ts
+++ server/routes/usersRouter.ts
@@ -3,7 +3,7 @@ import usersController from '../controllers/usersController';
 
 const router: Router = express.Router();
 router.post('/', usersController.authenticate, (req: Request,res: Response) => {
-  return res.status(200).send(JSON.stringify(res.locals.newUser)) // *** REPLACE PLACEHOLDER***
+  return res.status(200).send(JSON.stringify(res.locals.newUser || res.locals.foundUser)) // *** REPLACE PLACEHOLDER***
 })
 
 export default router;
