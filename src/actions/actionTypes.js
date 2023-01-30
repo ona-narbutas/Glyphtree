6871b31d0f3353d05f6f372ab3e58a6b4ae7462c
Ona Narbutas <ona.narbutas@gmail.com>
2023-01-29 20:53:59 -0500

    Merging from v2 repo

--- src/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const NEW_POST = 'NEW_POST';
-export const ENTER_TEXT = 'ENTER_TEXT';
-export const FEED = 'FEED';
-export const CONTINUE_TEXT = 'CONTINUE_TEXT';
-export const SUBMIT_CHILD = 'SUBMIT_CHILD';
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import postReducer from './postReducer';
-
-const reducers = combineReducers({
-  posts: postReducer
-})
-
-export default reducers;
