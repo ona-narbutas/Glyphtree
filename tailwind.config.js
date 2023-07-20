1eec769896b000d2ebfa9db1f7e2b8f124174611
Ona Narbutas <ona.narbutas@gmail.com>
2023-07-20 10:26:20 -0400

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/**/*.{html,js}', './dist/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
